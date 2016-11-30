b8424a57bd853e325c878a51b434c13efdec5ff8
GBabz <gabrielbabeux@gmail.com>
2016-11-29 21:49:31 -0500

    Added a timer and a Bunny death event handler

---
+++ Assets/Scripts/DestroyerOnHit.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyerOnHit : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+    }
+}
